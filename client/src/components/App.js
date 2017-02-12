6d750120ffe642416e5c21cf76c8ac7e66f28a69
Wendy <tell.wendy@gmail.com>
2017-02-11 22:22:50 -0800

    fix bug

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Router, IndexRoute, Route, Link, browserHistory } from 'react-router'
 import NavBar from './Navbar'
-import Explore from './Explore/index'
 import Home from './Home'
 
 class App extends React.Component {
