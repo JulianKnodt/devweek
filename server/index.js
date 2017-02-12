783a651b9862b57566db5b2ae1b1d3c26c6967eb
ZolayvarE <ZolayvarECU@gmail.com>
2017-02-11 21:30:55 -0800

    Add wildcard route

--- server/index.js
+++ server/index.js
@@ -38,6 +38,10 @@ app.get('/download', (req, res) => {
 
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'))
+})
+
 const port = process.env.PORT || 8080;
 app.listen(port, function () {
   console.log('Now listening on port:', port)
