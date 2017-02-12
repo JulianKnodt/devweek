f230cdabda338cf81d1a5b794bd1235c8a8e1f5e
edge <functaire@gmail.com>
2017-02-11 21:52:21 -0800

    format code

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div className="container">
-      <h1 className="header">Digital Marketplace for your projects</h1>
-      <img className="header-img" src="../assets/landing.jpg"/>
-      <div className="learnmore">Learn More</div>
-    </div>
-  )
+	return (
+		<div className="container">
+			<h1 className="header">Digital Marketplace for your projects</h1>
+			<img className="header-img" src="../assets/landing.jpg"/>
+			<div className="learnmore">Learn More</div>
+		</div>
+	)
 }
 
 export default Home
