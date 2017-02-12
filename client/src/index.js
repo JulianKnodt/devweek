825e63dd7f56eeee6848e17cc7a7618c48ab8deb
cassac <hicassac@gmail.com>
2017-02-11 21:13:51 -0800

    switch to hash histroy

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, IndexRoute, Route, Link, browserHistory } from 'react-router';
+import { Router, IndexRoute, Route, Link, hashHistory } from 'react-router';
 import App from './components/App.jsx';
 import NavBar from './components/Navbar.jsx';
 import Dashboard from './components/Dashboard/index.jsx'
@@ -8,7 +8,7 @@ import Home from './components/Home.jsx';
 
 ReactDOM.render(
 
-  <Router history={ browserHistory }>
+  <Router history={ hashHistory }>
     <Route path='/' component={ App }>
       <IndexRoute component={ Home } />
       <Route path='/explore' component={ Dashboard } />
