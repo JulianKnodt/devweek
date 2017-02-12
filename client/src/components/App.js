f21df69cb24644d4a89f94a28d8fd8c67739caf2
edge <functaire@gmail.com>
2017-02-11 21:17:49 -0800

    update project structure

---
+++ client/src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Router, IndexRoute, Route, Link, browserHistory } from 'react-router'
+import NavBar from './Navbar'
+import Explore from './Explore/index'
+import Home from './Home'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      message: 'I am a stateful component!'
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBar />
+        { this.props.children }
+      </div>
+    )
+  }
+
+}
+
+
+export default App
