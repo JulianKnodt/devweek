e8742d54a48189fc1e040d510021469b9a7762f1
ZolayvarE <ZolayvarECU@gmail.com>
2017-02-11 20:33:10 -0800

    Set up s3 uploading of files

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
+const s3 = require('./s3Handlers.js');
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client')));
---
+++ server/s3Handlers.js
@@ -0,0 +1,35 @@
+var path = require('path');
+
+var s3 = require('s3');
+
+var client = s3.createClient({
+  multipartUploadThreshold: 20971520,
+  multipartUploadSize: 15728640,
+  s3Options: {
+    accessKeyId: "AKIAJ2HSSE6BA3O2DCWQ",
+    secretAccessKey: "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",
+  },
+});
+
+module.exports = client;
+
+// var params = {
+//   localFile: path.join(__dirname, '../EricsResume.pdf'),
+ 
+//   s3Params: {
+//     Bucket: "purse-devweek",
+//     Key: "abc.pdf",
+//   },
+// };
+
+// var uploader = client.uploadFile(params);
+// uploader.on('error', function(err) {
+//   console.error("unable to upload:", err.stack);
+// });
+// uploader.on('progress', function() {
+//   console.log("progress", uploader.progressMd5Amount,
+//             uploader.progressAmount, uploader.progressTotal);
+// });
+// uploader.on('end', function() {
+//   console.log("done uploading");
+// });
